b90ddec658d16f5a27b7da555e8e5984a4506867
Raymond Santos <ragustinesantos@gmail.com>
2024-01-30 21:22:36 -0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/flutterproject/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterproject.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/alert_dialogue.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AlertDialogue extends StatelessWidget {
+  const AlertDialogue({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Error'),
+      
+    );
+  }
+}
